9c14cabd1a5268958db96c7652a1a3a4b1e66eb4
CuddlyShark <135768450+CuddlyShark-ds@users.noreply.github.com>
2023-08-06 10:59:38 -0400

    compare choices
    make function to compare the players selection
    with the computers random selection.

--- script.js
+++ script.js
@@ -6,4 +6,50 @@ function getComputerChoice() {
     return choices[computerSelection];
 }
 
-console.log(getComputerChoice());
+function playRound(computerSelection, playerSelection) {
+    switch(playerSelection) {
+        case "rock":
+            if(computerSelection === "rock"){
+                console.log("It's a tie")
+            }
+            else if(computerSelection === "paper"){
+                console.log("You loose!");
+            }
+            else{
+                console.log("You win!");
+            }
+            console.log("computer: " + computerSelection);
+            console.log("player: " + playerSelection);
+            break;
+        case "paper":
+            if(computerSelection === "rock"){
+                console.log("You win!")
+            }
+            else if(computerSelection === "paper"){
+                console.log("It's a tie!");
+            }
+            else{
+                console.log("You loose!");
+            }
+            console.log("computer: " + computerSelection);
+            console.log("player: " + playerSelection);
+            break;
+        case "scissors":
+            if(computerSelection === "rock"){
+                console.log("You loose!")
+            }
+            else if(computerSelection === "paper"){
+                console.log("You win!");
+            }
+            else{
+                console.log("It's a tie!");
+            }
+            console.log("computer: " + computerSelection);
+            console.log("player: " + playerSelection);
+            break;
+        default:
+            break;
+    }
+}
+
+playRound(getComputerChoice(), playerSelection);
