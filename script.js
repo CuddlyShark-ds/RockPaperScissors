ea7dda1fd7a160f9756c0dd160866f91e575ae08
CuddlyShark <135768450+CuddlyShark-ds@users.noreply.github.com>
2023-08-21 15:28:56 -0400

    Add UI interactions
    Return to the Rock Paper Scissors assignment to
    implement some UI functionality.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const choices = ["rock", "paper", "scissors"];
+const playerTextScore = document.getElementById("playerScore");
+const computerTextScore = document.getElementById("computerScore");
+const tieGameTextScore = document.getElementById("tieGames");
+const resultText = document.getElementById("results");
+
+let maxRounds = 5;
+let currentRound = 1;
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+
+playerTextScore.textContent = playerScore;
+computerTextScore.textContent = computerScore;
 
 function getComputerChoice() {
     let computerSelection = Math.floor(Math.random() * choices.length);
     return choices[computerSelection];
+}
+
+function playerSelection(choice) {
+    let playSelection = choice;
+    game(playSelection, getComputerChoice());
 }
 
 function playRound(computerSelection, playerSelection) {
@@ -45,42 +63,57 @@ function playRound(computerSelection, playerSelection) {
     }
 }
 
-function playerSelection() {
-    let selection = prompt("Please enter: rock, paper or scissors");
-    selection.toLocaleLowerCase();
+function game(playerChoice, computerChoice){
+    console.log(`player choice: ${playerChoice}, computer choice: ${computerChoice}`);
 
-    if(selection === "rock" || selection === "paper" || selection === "scissors"){
-        return selection;
+    let outCome = playRound(getComputerChoice(), playerChoice);
+
+    if(outCome === "win"){
+        console.log("You are the winner!");
+        playerTextScore.textContent = ++playerScore;
+    }
+    else if(outCome === "loss"){
+        console.log("You've lost!");
+        computerTextScore.textContent = ++computerScore;
     }
     else{
-        playerSelection();
+        console.log("It's a tie!")
+        tieGameTextScore.textContent = `number of games tied: ${++ties}`;
+    }
+
+    console.log(currentRound++, maxRounds);
+    if(currentRound > maxRounds){
+        announceWinner();
+        console.log("its winner time")
     }
 }
 
-function game(){
-    let rounds = 5;
-    let playerScore = 0;
-    let computerScore = 0;
-    let ties = 0;
+function announceWinner(){
+    let winner = Math.max(playerScore, computerScore);
+    let loser = Math.min(playerScore, computerScore);
+    let winnerContent = document.createElement("p");
 
-    for(let i = 0; i < rounds; i++) {
-        console.log("it's round: " + i);
-        let outCome = playRound(getComputerChoice(), playerSelection());
+    endGame();
 
-        if(outCome === "win"){
-            console.log("You are the winner!");
-            playerScore++;
-        }
-        else if(outCome === "loss"){
-            console.log("You've lost!");
-            computerScore++;
-        }
-        else{
-            console.log("It's a tie!")
-            ties++;
-        }
+    if(playerScore > computerScore){
+        winnerContent.textContent = `You've won with a score of:\nwins: ${playerScore}\nlosses: ${computerScore}\nties: ${ties}`;
     }
-    console.log(`The final score is you \n won: ${playerScore} rounds \n lost: ${computerScore} rounds \n tied: ${ties} rounds`)
+    else if(computerScore > playerScore){
+        winnerContent.textContent = `You've lost with a score of:\nwins: ${playerScore}\nlosses: ${computerScore}\nties: ${ties}`;
+    }
+    else if(computerScore === playerScore){
+        winnerContent.textContent = `It's a Tie game with a score of:\nwins: ${playerScore}\nlosses: ${computerScore}\nties: ${ties}`;
+    }
+    else{
+        winnerContent.textContent = `Error calculating a winner!!!`;
+    }
+
+    resultText.appendChild(winnerContent);
 }
 
-game();
+function endGame(){
+    const buttons = document.getElementsByTagName("button");
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].disabled = true;
+    }
+}
